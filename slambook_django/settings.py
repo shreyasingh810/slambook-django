f3c180bd53b90b2f2bbc522ac86905fb54e7e4f7
Shreya Singh <singh.shreya081098@gmail.com>
2019-08-09 00:37:38 +0530

    base.html complete

--- slambook_django/settings.py
+++ slambook_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'socialnetwork.apps.SocialnetworkConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
